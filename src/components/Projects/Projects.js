39c189e5751eb309c80e3c52ae3f057d04c57b88
Fernando <fernando.dorado.rueda@idener.es>
2022-09-13 22:37:26 +0200

    add projects work

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,10 +4,10 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
+import agricore from "../../Assets/Projects/agricore.jpg";
+import denim from "../../Assets/Projects/denim.png";
 import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import heroes from "../../Assets/Projects/logo-heroes.png";
 
 function Projects() {
   return (
@@ -23,68 +23,34 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={denim}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="DENiM"
+              description="The EU-funded DENiM project is developing an integrated toolchain for the provision of advanced digital services including secure-edge connectivity leveraging the Internet of Things (IoT), data analytics, digital twin, energy modelling and automation. Digital technologies will play a significant role by providing the ability to automatically monitor and optimise energy usage, while continuously informing users about the environmental and economic impact of decisions made at all stages of the manufacturing process."
+              ghLink="https://gitlab.com/denim-project"
+              // demoLink="https://chatify-49.web.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={heroes}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="HEROES"
+              description="The number of people, including children, falling victim to human trafficking and child sexual abuse and exploitation (CSA/CSE) around the world continues to grow, despite increased international attention and resources. In Europe, sexually exploited women and girls make up the majority of human trafficking victims. Moreover, assistance to victims is often limited by the lack of coordination among stakeholders. In this context, the EU-funded HEROES project will explore how to use the latest technological advances and new strategies to prevent and combat CSA/CSE and human trafficking, investe the crimes and better protect victims. It will develop an ambitious, interdisciplinary, international and victim-centred approach. Its aim is to establish a coordinated contribution with law enforcement agencies to address the specific needs of victims and provide protection."
+              ghLink="https://heroes-fct.es/"
+              // demoLink="https://blogs.soumya-jit.tech/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={agricore}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              title="AGRICORE"
+              description="The EU-funded AGRICORE project aims to use an agent-based approach to improve on traditional methods with modelling and information and communication technology (ICT). It will use computational modelling to simulate farmers’ actions as autonomous or collective entities, with contexts ranging from local to global scales. Artificial intelligence, Big Data, cloud services will be involved in the process. This open source tool will permit more efficient, optimised policies with its predictive and monitoring capabilities while ensuring transparency and constant improvement."
+              ghLink="https://agricore-project.eu/"
+              // demoLink="https://editor.soumya-jit.tech/"              
             />
           </Col>
         </Row>
