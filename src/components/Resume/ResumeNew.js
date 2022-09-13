d64310cb179d33e1668a5c5e8c38cabf9cd93f15
Fernando <fernando.dorado.rueda@idener.es>
2022-09-13 22:28:32 +0200

    modify cv link

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+import pdf from "../../Assets/../Assets/FernandoCV2022.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://1drv.ms/b/s!Ah3OEV1FcETliYwAtgNSIDYtSrytOA?e=5M1cML";
+  "https://raw.githubusercontent.com/FernandoDorado/portfolio/master/src/Assets/FernandoCV2022.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
